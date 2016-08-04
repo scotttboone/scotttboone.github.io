c996998ed0c934015f19ce0a632427c20d6c8051
Scott Boone <scotttboone@gmail.com>
2016-08-04 14:17:40 -0500

    sdf

--- bikemap/js/main.js
+++ bikemap/js/main.js
@@ -14,6 +14,19 @@ if(window.attachEvent) {
     }
 }   
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.")
+    }
+}
+
+function showPosition(position) {
+    console.log(position.coords.latitude + '; ' + position.coords.longitude) 
+}
+
+
 //load background map
 function loadMapbox() {
 	mapboxgl.accessToken = 'pk.eyJ1Ijoic2NvdHR0Ym9vbmUiLCJhIjoiY2lsdjB3NHM4MDFocnZka3NvZ3QydDQydyJ9.Wy_MJGjGTCbcg4iNm7ffJg';
@@ -24,6 +37,7 @@ function loadMapbox() {
 	      zoom: [14]
 	  });
 }
+
 
 //map should load data for bounds on map movement? maybe with delay?
 
@@ -41,25 +55,32 @@ function loadOSMData() {
         map.getBounds().getWest() +','+
         map.getBounds().getNorth() +','+
         map.getBounds().getEast() +')'
+    
+    var myCenter = map.getCenter()
+    var myRadius = 1000
+
 
     //mybounds - '(30.250879189641353,-97.73687460327172,30.273119551100805,-97.71112539672875)'
     var myOverpassQuery = 
+
+            // server
             'http://overpass.osm.rambler.ru/cgi/interpreter?data='+
             //'https://overpass-api.de/api/interpreter?data=' + 
+
+            // output type
             '[out:json];' +
+
             '('+
-                'way[highway="residential"]' + mybounds + ';' +
-                'way[highway="tertiary"]' + mybounds + ';' +
-                'way[highway="unclassified"]' + mybounds + ';' +
-                'way[highway="cycleway"]' + mybounds + ';' +
-                'way[highway="footway"]' + mybounds + ';' +
-                'way[highway="living_street"]' + mybounds + ';' +
-                'way[highway="pedestrian"]' + mybounds + ';' +
-                'way[highway="service"]' + mybounds + ';' +
-                'way[cycleway]' + mybounds + ';' + //cycleways not starting with no/none
+                // geo radius
+                'way' + '(around:' + myRadius + ',' + myCenter.lat + ',' + myCenter.lng + ')' +
+                // segement types
+                '[~"highway|cycleway"~"residential|tertiary|unclassified|cycleway|footway|living_street|pedestrian|service"]' +
             ');'+
             '(._;>;);'+ //include all nodes recursively
-            'out;'
+
+            // output type
+            'out skel;'
+
 
     document.getElementById('debug').innerHTML = myOverpassQuery
     console.log(myOverpassQuery)
@@ -71,39 +92,8 @@ function loadOSMData() {
         type: 'GET',
         async: true,
         crossDomain: true, 
-        //success: function(json) {
-        //    console.log(JSON.stringify(osmtogeojson(json)));
-        //},
-        success: function(json) {
-            console.log(JSON.stringify(osmtogeojson(json)));
-            
-            // convert map data into geojson format
-            var mapdata = new mapboxgl.GeoJSONSource({
-                data: osmtogeojson(json)
-            });
+        success: plotData
 
-             // add source
-            try{map.removeSource('path');}catch(err){}
-            map.addSource('path', mapdata);
-
-            // show source on map
-            map.addLayer({
-                "id": "path",
-                "type": "line",
-                "source": "path",
-                "layout": {
-                    "line-join": "round",
-                    "line-cap": "round"
-                },
-                "paint": {
-                    "line-color": "#ff00ff",
-                    "line-width": 2
-                }
-
-            });
-
-            
-        },
     }).done(function() {
         console.log( "second success" );
         //document.getElementById('mybutton').innerHTML = "loading..."
@@ -117,5 +107,31 @@ function loadOSMData() {
         document.getElementById('mybutton').innerHTML = "where the burgers at "
     });
 
+};
+
+function plotData(json) {
+    console.log(JSON.stringify(osmtogeojson(json)));
+    
+    // convert map data into geojson format
+    var mapdata = new mapboxgl.GeoJSONSource({
+        data: osmtogeojson(json)
+    });
+     // add source
+    try{map.removeSource('path');}catch(err){}
+    map.addSource('path', mapdata);
+    // show source on map
+    map.addLayer({
+        "id": "path",
+        "type": "line",
+        "source": "path",
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#ff00ff",
+            "line-width": 2
+        }   
+    });
 
 };
